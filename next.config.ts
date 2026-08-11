2833eac21b983a9d303f718e4b94c06a475ed83d
SY <SY-NEW@users.noreply.github.com>
2026-08-12 04:09:04 +0800

    Fix Vercel deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: "/my-profile",
-  assetPrefix: "/my-profile/",
 };
 
 export default nextConfig;
